19ee6bf6c6d1f1ebf63607fed3d4d1714be14a7c
matthewbrice5 <matthewbrice5@gmail.com>
2026-08-28 15:16:19 -0500

    Add files via upload

--- GalacaGame.cpp
+++ GalacaGame.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
 #include "raylib.h"
-#include "Bullet.h"
+
+
+struct Bullet{
+    int x;
+    int y;
+    
+};
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -21,10 +28,20 @@ int main(void)
     
 
     InitWindow(screenWidth, screenHeight, "Galaca Game - Singplayer");
+    Image icon = LoadImage("user.png");
+    SetWindowIcon(icon);
+    UnloadImage(icon);
+    
+    
+    
     
     //Load in all textures 
     Texture2D usersprite = LoadTexture("user.png");
+    Texture2D currentusersprite; //This gets changed during the code for the users tilt left and right
     Texture2D background = LoadTexture("background.png");
+    Texture2D bulletimage = LoadTexture("bullet.png");
+    
+    std::vector<Bullet> firingbullets;
     
     
 
@@ -47,43 +64,69 @@ int main(void)
         
         //MOVEMENT
         if (IsKeyDown(KEY_A)) {// This code runs only once each frame, when a button is pressed 
-            if (userpositionx != 0){
+            if (userpositionx != 0){// Sets the border ie. cannot go too far left
             userpositionx-= speed;
             }
             
-        } else if (IsKeyDown(KEY_D)){
+        } else if (IsKeyDown(KEY_D)){// Sets the border ie. cannot go too far right
             if (userpositionx != 530){
             userpositionx+= speed;
             }
         }
         //SHOOTING
         if (IsMouseButtonPressed(0)){
-            //When the mouse is pressed, create a new class with the bullet x and y being (35,17) from the users
+            //When the mouse is pressed, create a new strcut with the bullet x and y being (35,17) from the users
             // then update the y axis by increasing by 8 everytime after
             
             
-            //Bullet bullet1(userpositionx + 35,userpositiony-17);
+            Bullet newbullet;
+            newbullet.x = userpositionx + 35;
+            newbullet.y = userpositiony - 17;
+            
+            firingbullets.push_back(newbullet); //Adds the struct to a vector, that gets displayed later
+            
             
         }
         
         
         //-----------------------------------------
         
-        backgroundy--;
+        
         
         
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
-
+        backgroundy--; // Moves the background down
+        
+        
+        
+        
+        
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            DrawTexture(background,0,backgroundy,WHITE);
-
-            DrawText("Galaca!", 100, 200, 40, WHITE);
+            DrawTexture(background,0,backgroundy,WHITE);//Draws the starts 
             
             DrawTexture(usersprite, userpositionx, userpositiony, WHITE);//Draws users position
+            
+            //loop that draws all bullets
+            for(size_t i = 0; i < firingbullets.size(); i++){
+                
+                if (firingbullets[i].y < 5){
+                    firingbullets.erase(firingbullets.begin() + i);
+                }
+                
+                
+                DrawTexture(bulletimage, firingbullets[i].x, firingbullets[i].y, WHITE);
+            
+                
+                firingbullets[i].y -= 8;
+                
+                
+            
+            
+            }
             
             
             
