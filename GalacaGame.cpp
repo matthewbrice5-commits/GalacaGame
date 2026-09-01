c7b762c2fac4d598ac56f53949fc5c96d37d6510
matthewbrice5 <matthewbrice5@gmail.com>
2026-09-01 09:46:05 -0500

    Add files via upload

--- GalacaGame.cpp
+++ GalacaGame.cpp
@@ -3,11 +3,29 @@
 #include "raylib.h"
 
 
-struct Bullet{
+struct Bullet{  //Keeps track of the bullets x and y coordiante 
     int x;
     int y;
     
 };
+
+struct BasicEnemy{ // Holds the top left and bottom right of the image, with a method 
+                   // that tells the enemy how to move
+    int x;
+    int y;
+    int bottomx;
+    int bottomy;
+    
+    void updateframe(){
+        y += 2;
+    }
+    
+    
+};
+
+
+
+
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -37,11 +55,35 @@ int main(void)
     
     //Load in all textures 
     Texture2D usersprite = LoadTexture("user.png");
-    Texture2D currentusersprite; //This gets changed during the code for the users tilt left and right
+    Texture2D userspriteleft = LoadTexture("userleft.png");
+    Texture2D userspriteright = LoadTexture("userright.png");
+    Texture2D currentusersprite = usersprite;; //This gets changed during the code for the users tilt left and right
     Texture2D background = LoadTexture("background.png");
     Texture2D bulletimage = LoadTexture("bullet.png");
+    Texture2D enemyimage = LoadTexture("enemy1.png");
+    Texture2D explosion = LoadTexture("explosion.png");
     
-    std::vector<Bullet> firingbullets;
+    std::vector<Bullet> firingbullets; //This vector holds all active bullets on the screenHeight
+    
+    // This holds enemy positions for the first wave
+    std::vector<BasicEnemy> level1;
+    int enemypos = 100;
+    
+    //fills first level with enemies 
+    for(int i = 0; i < 6; i++){
+        BasicEnemy enemy;
+        enemy.x = enemypos;
+        enemy.y = 00;
+        enemy.bottomx = enemy.x+49;
+        enemy.bottomy = enemy.y+49;
+        
+        
+        
+        level1.push_back(enemy);
+        enemypos+=75;
+        
+    }
+    
     
     
 
@@ -66,12 +108,16 @@ int main(void)
         if (IsKeyDown(KEY_A)) {// This code runs only once each frame, when a button is pressed 
             if (userpositionx != 0){// Sets the border ie. cannot go too far left
             userpositionx-= speed;
+            currentusersprite = userspriteleft;
             }
             
         } else if (IsKeyDown(KEY_D)){// Sets the border ie. cannot go too far right
             if (userpositionx != 530){
             userpositionx+= speed;
+            currentusersprite = userspriteright;
             }
+        } else {
+            currentusersprite = usersprite;
         }
         //SHOOTING
         if (IsMouseButtonPressed(0)){
@@ -108,25 +154,52 @@ int main(void)
 
             DrawTexture(background,0,backgroundy,WHITE);//Draws the starts 
             
-            DrawTexture(usersprite, userpositionx, userpositiony, WHITE);//Draws users position
+            DrawTexture(currentusersprite, userpositionx, userpositiony, WHITE);//Draws users position
             
             //loop that draws all bullets
             for(size_t i = 0; i < firingbullets.size(); i++){
                 
-                if (firingbullets[i].y < 5){
+                if (firingbullets[i].y < 5){ //If the bullet goes out of bounds, offload it
                     firingbullets.erase(firingbullets.begin() + i);
                 }
                 
-                
-                DrawTexture(bulletimage, firingbullets[i].x, firingbullets[i].y, WHITE);
-            
-                
                 firingbullets[i].y -= 8;
-                
-                
-            
-            
+                DrawTexture(bulletimage, firingbullets[i].x, firingbullets[i].y, WHITE);   
+
+               
             }
+            
+            //loop that draws all enemies in level1
+            for(int i = (int)level1.size() - 1; i >= 0; i--){
+                bool hit = false;
+                
+                
+                
+                for(int j = (int)firingbullets.size() - 1; j >= 0; j--){
+                    
+                    if(firingbullets[j].x > level1[i].x && firingbullets[j].x < level1[i].bottomx && firingbullets[j].y > level1[i].y && firingbullets[j].y < level1[i].bottomy){
+                        
+                        hit = true;
+                        DrawText("Hit!", 100, 100, 20, RED);
+                        firingbullets.erase(firingbullets.begin() + j);
+                        break;
+                        
+                    }
+                    
+                }
+              
+                if(!hit) {  // Only draw and update if NOT hit
+                    DrawTexture(enemyimage, level1[i].x, level1[i].y, WHITE);
+                    level1[i].updateframe();
+                } else {
+                    // Draw explosion here if you want
+                    level1.erase(level1.begin() + i);
+                }
+                    
+                
+            }
+            
+            
             
             
             
